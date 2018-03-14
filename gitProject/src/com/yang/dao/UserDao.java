2eab44e50b83f0ea4a29a12e2c7b9d474652ef75
yubin <seoyubin12@gmail.com>
2018-03-14 16:14:46 +0900

    ver 4

--- gitProject/src/com/yang/dao/UserDao.java
+++ gitProject/src/com/yang/dao/UserDao.java
@@ -11,8 +11,8 @@ public class UserDao {
 	
 	public UserDao() {
 		list = new ArrayList<>();
-		list.add(new User("홍길동",17,75,3));
-		list.add(new User("이순신",18,100,0));
-		list.add(new User("강호동",15,90,3));
+		list.add(new User("홍길동", 17, 60.3));
+		list.add(new User("이순신",18,100.0));
+		list.add(new User("강호동",15,90.3));
 	}
 }
