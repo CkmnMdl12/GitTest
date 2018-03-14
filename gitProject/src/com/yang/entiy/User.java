59e7640442cce31f7a3bbee7afd9364e601197f2
yubin <seoyubin12@gmail.com>
2018-03-14 16:09:50 +0900

    ver 4

--- gitProject/src/com/yang/entiy/User.java
+++ gitProject/src/com/yang/entiy/User.java
@@ -5,5 +5,34 @@ public class User {
 	private String name;
 	private int age;
 	private double score;
+	public User() {
+		super();
+	}
+	public User(String name, int age, double score) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.score = score;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	
+	
 
 }
--- gitProject/src/com/yang/entiy/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.yang.entiy;
-
-public class UserDao {
-
-	public static void main(String[] args) {
-		System.out.println("Userdao");
-	}
-
-}
